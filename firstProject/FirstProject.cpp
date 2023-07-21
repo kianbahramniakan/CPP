d017a054997a1c4e9c976b5db33302a653049c35
DESKTOP-6SC0T2O\Asus <bahram.niakna.kian@gmail.com>
2023-07-21 12:45:26 +0330

    creating string

--- firstProject/FirstProject.cpp
+++ firstProject/FirstProject.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    int firstNumber = 0;
-    int secondNumber = 0;
-
-    std::cout<<"please enter first number: ";
-    std::cin>> firstNumber;
-
-    std::cout<<"please enter second number: ";
-    std::cin>> secondNumber;
-
-    int sum = firstNumber + secondNumber;
-    int sub = firstNumber - secondNumber;
-    int mul = firstNumber * secondNumber;
-    int dev = firstNumber / secondNumber;
-
-    std::cout<<"Sum : "<<sum <<std::endl <<"Sub : "<<sub <<std::endl <<"Mul : "<<mul <<std::endl <<"Dev : "<<dev <<std::endl;
-
-//    std::cin.get()
+    std::cout<<"Hello, Whats your name? ";
+    std::string name;
+    std::getline(std::cin, name);
+    std::cout<<"hello "+name;
     return 0;
 }
